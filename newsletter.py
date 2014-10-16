1e84154aebfa2c62231accea0e3f6a48b5a1ee74
resteve <resteve@zikzakmedia.com>
2014-10-16 09:33:11 +0200

    Contact lang

--- newsletter.py
+++ newsletter.py
@@ -2,6 +2,8 @@
 #The COPYRIGHT file at the top level of this repository contains 
 #the full copyright notices and license terms.
 from trytond.model import ModelView, ModelSQL, fields
+from trytond.pool import Pool
+from trytond.transaction import Transaction
 
 __all__ = ['NewsletterList', 'NewsletterContact', 'NewsletterContactList']
 
@@ -29,6 +31,7 @@ class NewsletterContact(ModelSQL, ModelView):
     lists = fields.Many2Many('newsletter.contact-newsletter.list',
         'newsletter_contact', 'newsletter_list', 'Lists')
     active = fields.Boolean('Active', select=True)
+    lang = fields.Many2One("ir.lang", 'Language')
 
     @classmethod
     def __setup__(cls):
@@ -44,6 +47,21 @@ class NewsletterContact(ModelSQL, ModelView):
     @staticmethod
     def default_active():
         return True
+
+    @staticmethod
+    def default_lang():
+        pool = Pool()
+        Configuration = pool.get('party.configuration')
+        Lang = pool.get('ir.lang')
+
+        if Transaction().language:
+            lang, = Lang.search([
+                ('code', '=', Transaction().language),
+                ], limit=1)
+            return lang.id
+        config = Configuration(1)
+        if config.party_lang:
+            return config.party_lang.id
 
     @classmethod
     def copy(cls, contacts, default=None):
